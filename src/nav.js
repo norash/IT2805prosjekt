4abc64f3a8e4d8fd9b20206a12bc6dae1753cf20
norash <norahaugerud@gmail.com>
2015-11-06 14:43:32 +0100

    lagt til navigasjon i template

---
+++ src/nav.js
@@ -0,0 +1,9 @@
+function showMenu(id) {
+	var menu = document.getElementById(id);
+	menu.style.display = "block";
+}
+
+function hideMenu(id) {
+	var menu = document.getElementById(id);
+	menu.style.display = "none";
+}
