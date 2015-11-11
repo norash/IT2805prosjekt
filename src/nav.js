43a81bb10bbfff32cd8541a43e2ae93bdf4d0a39
norash <norahaugerud@gmail.com>
2015-11-11 08:07:19 +0100

    Endret på nav

--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,8 @@
 function showMenu(id) {
 	var menu = document.getElementById(id);
 	menu.style.display = "block";
+	// var btn = document.getElementById("salgBtn");
+	// btn.style.backgroundColor = "#B3A580";
 }
 
 function hideMenu(id) {
