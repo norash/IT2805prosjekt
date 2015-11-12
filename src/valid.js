caec085cd6178c12e543caab2f80b74b9c69634e
Jonaseth <jonaseth@stud.ntnu.no>
2015-11-12 20:24:45 +0100

    La til valid.js og forbedret kontakt.html sin css

--- src/valid.js
+++ src/valid.js
@@ -6,26 +6,39 @@ PURPOSE: JavaScript for form validation
 */
 
 
-document.getElementById("send").addEventListener('click',function(){
-	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	if(tlfInput.value.length==0 && epostInput.value.length==0){
-		document.getElementById("warning").innerHTML = "Vær snill å oppgi enten e-postadresse eller telefonnummer <br> så vi kan svare deg.";
-		return false;
-	}
-	else{
-		document.getElementById("warning").innerHTML = "";  
-		if(epostInput.value.match(mailformat)){
-			epostInput.focus();  
-			return true;  
-		} 
-		else if(epostInput.value.length==0){
-			epostInput.focus();
-			return true;
-		}
-		else{  
-			document.getElementById("warning").innerHTML = "Hei";  
-			epostInput.focus();  
-			return false;  
-		}  
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var numberformat = /^[0-9,]+$/;
+
+document.getElementById("send").addEventListener('click',function(event){
+	document.getElementById("navnInput").style.borderColor = "#453823";
+	document.getElementById("tlfInput").style.borderColor = "#453823";
+	document.getElementById("epostInput").style.borderColor = "#453823";
+	document.getElementById("postnr").style.borderColor = "#453823";
+	document.getElementById("foresporselInput").style.borderColor = "#453823";
+	if(navnInput.value.length===0){
+		document.getElementById("warning").innerHTML = "Vennligst skriv inn navnet ditt.";
+		document.getElementById("navnInput").style.borderColor = "red";
+		event.preventDefault();
+	}else if(tlfInput.value.length===0 && epostInput.value.length===0){
+		document.getElementById("warning").innerHTML = "Vennligst oppgi enten e-postadresse eller telefonnummer <br> så vi kan svare deg.";
+		document.getElementById("tlfInput").style.borderColor = "red";
+		document.getElementById("epostInput").style.borderColor = "red";
+		event.preventDefault();
+	}else if(tlfInput.value.match(numberformat)==null && tlfInput.value.length!==0){
+		document.getElementById("warning").innerHTML = "Dette er ikke et gyldig telefonnummer.";
+		document.getElementById("tlfInput").style.borderColor = "red";
+		event.preventDefault();
+	}else if(epostInput.value.match(mailformat)==null && epostInput.value.length!==0){
+		document.getElementById("warning").innerHTML = "Dette er ikke en gyldig epost-adresse.";
+		document.getElementById("epostInput").style.borderColor = "red";
+		event.preventDefault();
+	}else if(postnr.value.match(numberformat)==null && postnr.value.length!==0){
+		document.getElementById("warning").innerHTML = "Postnummeret kan bare inneholde tall.";
+		document.getElementById("postnr").style.borderColor = "red";
+		event.preventDefault();
+	}else if(foresporselInput.value==''){
+		document.getElementById("warning").innerHTML = "Vennligst skriv hva vi kan hjelpe deg med i forespørsel-feltet.";
+		document.getElementById("foresporselInput").style.borderColor = "red";
+		event.preventDefault();
 	}
 });
