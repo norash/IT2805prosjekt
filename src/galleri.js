9bd155d1c9cdf0591e61e36f519dd7e27a247b79
Jonaseth <jonaseth@stud.ntnu.no>
2015-11-14 17:53:14 +0100

    Bildegalleri
    Fikset litt på CSSen til bildergallery.html, og gjorde JavaScripten om
    til et eget dokument. La tilbake et bilde som hadde blitt slettet.

---
+++ src/galleri.js
@@ -0,0 +1,3 @@
+function op1(pic){
+	window.open(pic,"_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top='(window.screen.height - 600) / 2 + ', left='+ (window.screen.width - 800) / 2 +', width=700, height=850");
+}
